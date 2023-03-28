e6908dacbebf795f170794d2f492d74cb3d2f3cf
avivashishta29 <avivashishta@gmail.com>
2023-03-28 22:39:49 +0530

    react intro

---
+++ src/MyComponent.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function MyComponent() {
+  return (
+    <div>
+      <h1>Hi This is A Component</h1>
+      <p></p>
+    </div>
+  );
+}
+
+export default MyComponent;
---
+++ src/MyHeader.js
@@ -0,0 +1,9 @@
+function MyHeader() {
+  return (
+    <div id="my-header">
+      <p>This is my header</p>
+    </div>
+  );
+}
+
+export default MyHeader;
