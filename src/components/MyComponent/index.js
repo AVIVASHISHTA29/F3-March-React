8494cdd906d663feacafebcadfdcb21506179246
avivashishta29 <avivashishta@gmail.com>
2023-03-29 22:19:07 +0530

    functions

--- src/components/MyComponent/index.js
+++ src/components/MyComponent/index.js
@@ -2,10 +2,41 @@ import React from "react";
 import "./styles.css";
 
 function MyComponent() {
+  var name = "Avi Vashishta";
+  var num = 10;
+
+  function sayHello() {
+    console.log("Hello World");
+  }
+
+  function MouseUpFnc() {
+    console.log("Mouse Up");
+  }
+
+  function MouseDownFnc() {
+    console.log("Mouse Down");
+  }
+
+  function MouseHover() {
+    console.log("Mouse Hover");
+  }
+
+  // How do i call my JS functions inside html
+
   return (
     <div className="component-wrapper">
-      <h1>Hi This is A Component</h1>
-      <p></p>
+      <h1>Hi My Name is {name.length == 3 ? "if" : "else"}</h1>
+      <h1>{(num + 10) * 1000}</h1>
+      <p style={{ fontWeight: "bold" }}>Styled P tag</p>
+      <button
+        onClick={sayHello}
+        // onMouseUp={(e) => MouseUpFnc()}
+        // onMouseDown={MouseDownFnc}
+        onMouseOver={() => MouseHover()}
+      >
+        Click Me
+      </button>
+      <input onChange={(e) => console.log(e.target.value)} />
     </div>
   );
 }
